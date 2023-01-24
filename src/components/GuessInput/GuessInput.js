9e3ee7f170ddc99b263bd18d08aa0a15af4a1666
Kaz de Groot <kaz@kdds.nl>
2023-01-25 00:06:06 +0100

    Bit of security

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -4,6 +4,12 @@ function GuessInput({ addGuess }) {
   const [guess, setGuess] = useState('');
   function onSubmit(e) {
     e.preventDefault();
+
+    if (guess.length !== 5) {
+      alert('Needs to be 5 chars...');
+      return;
+    }
+
     addGuess(guess);
     setGuess('');
   }
